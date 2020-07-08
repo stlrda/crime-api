b4850bf0b58edabccb4ee145cc683da4ea3b60ae
bransonf <branson.fox@slu.edu>
2020-07-07 19:27:04 -0500

    Update env vars in scrape

--- scrape/scrape.R
+++ scrape/scrape.R
@@ -16,8 +16,8 @@ con <- DBI::dbConnect(
   dbname = Sys.getenv('DB_NAME'),
   host = Sys.getenv('DB_HOST'),
   port = Sys.getenv('DB_PORT'),
-  user = Sys.getenv('USER'),
-  password = Sys.getenv('PASS')
+  user = Sys.getenv('DB_USER'),
+  password = Sys.getenv('DB_PASS')
 )
 
 # Optionally Auth Pushover
